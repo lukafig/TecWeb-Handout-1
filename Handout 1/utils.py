623e5cbcee2f722d3aaa524525392cd8a200142d
lukafig <72396320+lukafig@users.noreply.github.com>
2024-02-14 17:20:46 -0300

    Add files via upload

---
+++ Handout 1/utils.py
@@ -0,0 +1,32 @@
+import json
+def extract_route(string):
+    #route = string.split('/', 1)[1].split(' ', 1)[0]
+    route = string.split()[1][1:]
+    return route
+
+def read_file(path):
+    with open(path, 'rb') as file:
+        content = file.read()
+    return content
+
+def load_data(file_name):
+    with open("data/" + file_name, 'r') as file:
+        data = json.load(file)
+    return data
+
+def load_template(file_name):
+    with open("templates/" + file_name, 'r') as file:
+        template = file.read()
+    return template
+
+def add_note(note):
+    with open("data/notes.json", 'r+') as file:
+        data = json.load(file)
+        data.append(note)
+        file.seek(0)
+        json.dump(data, file, indent=4)
+        file.truncate()
+
+def build_response(body='', code=200, reason='OK', headers=''):
+    response = f'HTTP/1.1 {code} {reason}\n{headers}\n{body}'
+    return response.encode()
---
+++ Handout 1/views.py
@@ -0,0 +1,31 @@
+from utils import load_data, load_template, add_note
+import urllib.parse
+
+
+def index(request):
+    # A string de request sempre começa com o tipo da requisição (ex: GET, POST)
+
+    if request.startswith('POST'):
+        request = request.replace('\r', '')  # Remove caracteres indesejados
+        # Cabeçalho e corpo estão sempre separados por duas quebras de linha
+        partes = request.split('\n\n')
+        corpo = partes[1]
+        params = {}
+        # Preencha o dicionário params com as informações do corpo da requisição
+        # O dicionário conterá dois valores, o título e a descrição.
+        # Posteriormente pode ser interessante criar uma função que recebe a
+        # requisição e devolve os parâmetros para desacoplar esta lógica.
+        # Dica: use o método split da string e a função unquote_plus
+        for chave_valor in corpo.split('&'):
+            chave, valor = chave_valor.split('=')
+            params[chave] = urllib.parse.unquote_plus(valor)
+        add_note(params)
+
+    note_template = load_template('components/note.html')
+    notes_li = [
+        note_template.format(title=dados['titulo'], details=dados['detalhes'])
+        for dados in load_data('notes.json')
+    ]
+    notes = '\n'.join(notes_li)
+
+    return load_template('index.html').format(notes=notes).encode()
